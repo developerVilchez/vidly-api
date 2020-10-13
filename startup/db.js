6c09dcfa7575cf5c5456218e8f16f0ff7f477de6
developervilchez <developervilchez@gmail.com>
2020-10-12 22:44:37 -0500

    delete string to private db key comment

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,6 @@ const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
 
-
 module.exports = function() {
     const db = config.get('db');
   mongoose.connect(db)
