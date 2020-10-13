ec099809732626edc09e94cd206a007f6f3672ff
developervilchez <developervilchez@gmail.com>
2020-10-13 00:23:08 -0500

    delete string api key mongo db sources

--- startup/db.js
+++ startup/db.js
@@ -4,8 +4,7 @@ const config = require('config');
 
 
 module.exports = function() {
-    const db = config.get('db');
-/*   const db = "mongodb://prueba:prueba123@cluster0-shard-00-00.udkdg.mongodb.net:27017,cluster0-shard-00-01.udkdg.mongodb.net:27017,cluster0-shard-00-02.udkdg.mongodb.net:27017/vidly_staging?ssl=true&replicaSet=atlas-8ml4s5-shard-0&authSource=admin&retryWrites=true&w=majority" */
+  const db = config.get('db');
   mongoose.connect(db)
     .then(() => winston.info(`Connected to mongo cloud`))
     .catch((err) => console.log(err) )
